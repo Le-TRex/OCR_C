bd63cbe94542905110c3c28d0d2abb89171febdc
Le-TRex <clarembo@gmail.com>
2021-04-19 16:50:50 +0200

    Jeu du nombre mystère avec possibilité de rejouer

--- main.c
+++ main.c
@@ -14,27 +14,45 @@ int main (int argc, char **argv[]) {
     int chosenNumber;
     int numberOfTrials = 0;
 
+    int userChoice;
+    _Bool tryAgain = 1;
+
     srand(time(NULL)); //permet l'utilisation de la fonction rand()
 
-    mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
-
     printf("\t## Bienvenue dans le jeu du nombre mystère ! ##\n");
-    printf("\t## Quel est le nombre ? (entre 1 et 100) ##\n");
 
-    do {
-        numberOfTrials ++ ;
-        scanf("%d", &chosenNumber);
+    while (tryAgain != 0) {
+        mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
 
-        if ( chosenNumber < 1 || chosenNumber >100) {
-            printf("\t## Le nombre %d n'est pas compris entre 1 et 100. Réessayez :)##\n", chosenNumber);
-        } else if (chosenNumber > mysteryNumber) {
-            printf("\t## Le nombre mystère est plus petit ! Réessayez :) ##\n");
-        } else if (chosenNumber < mysteryNumber) {
-            printf("\t## Le nombre mystère est plus grand ! Réessayez :) ##\n");
-        } else if (chosenNumber == mysteryNumber) {
-            printf("\t## BRAVO ! En %d coups, vous avez découvert le nombre mystère : %d ! ##\n", numberOfTrials, mysteryNumber);
+        printf("\t## Quel est le nombre ? (entre 1 et 100) ##\n");
+
+        do {
+            numberOfTrials ++ ;
+            scanf("%d", &chosenNumber);
+
+            if ( chosenNumber < 1 || chosenNumber >100) {
+                printf("\t## Le nombre %d n'est pas compris entre 1 et 100. Réessayez :)##\n", chosenNumber);
+            } else if (chosenNumber > mysteryNumber) {
+                printf("\t## Le nombre mystère est plus petit ! Réessayez :) ##\n");
+            } else if (chosenNumber < mysteryNumber) {
+                printf("\t## Le nombre mystère est plus grand ! Réessayez :) ##\n");
+            } else if (chosenNumber == mysteryNumber) {
+                printf("\t## BRAVO ! En %d coups, vous avez découvert le nombre mystère : %d ! ##\n", numberOfTrials, mysteryNumber);
+            }
+        } while (chosenNumber != mysteryNumber);
+
+        printf("\t## Souhaitez vous refaire une partie ? ##\n"
+               "\t## 1 : oui ##\n"
+               "\t## 0 : non ##\n");
+
+        scanf("%d", &userChoice);
+        if (userChoice < 0 || userChoice > 1){
+            printf("\t## la réponse n'était ni 0 ni 1, veuillez rééssayer :) ##");
+        } else if (userChoice == 0){
+            tryAgain = 0;
         }
-    } while (chosenNumber != mysteryNumber);
+    }
+
 
     return 0;
 }
